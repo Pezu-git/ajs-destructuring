9452f92782f408df201b0f4890ca38055fcffcdc
Pezu-git <artem_nikolaev69@mail.ru>
2021-03-03 16:10:41 +0300

    fixed

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-unused-vars
 import description from '../js/app.js';
+import { character } from '../js/character.js';
 
 test('first test', () => {
   const bowman = [
@@ -18,4 +19,55 @@ test('first test', () => {
     },
   ];
   expect(description('Bowman')).toEqual(bowman);
+});
+test('second test', () => {
+  function pushChar() {
+    character.special.push(
+      {
+        id: 10,
+        name: 'Выстрел',
+        icon: 'http://...',
+        description: 'Наносит обычный урон',
+      },
+      {
+        id: 11,
+        name: 'Добивающий удар',
+        icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+      },
+    );
+  }
+  pushChar();
+  const bowman = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+    {
+      id: 10,
+      name: 'Выстрел',
+      icon: 'http://...',
+      description: 'Наносит обычный урон',
+    },
+    {
+      id: 11,
+      name: 'Добивающий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+  expect(description('Bowman')).toEqual(bowman);
+});
+test('wrongChar test', () => {
+  expect(() => {
+    description('Bowmann');
+  }).toThrow('Ошибка');
 });
--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,16 @@ import { character } from './character.js';
 
 export default function description(pers) {
   // eslint-disable-next-line eqeqeq
-  // if (pers === character.type) {
   const { special } = character;
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < special.length; i++) {
-    // eslint-disable-next-line eqeqeq
-    if (special[i].description == undefined) {
-      special[i].description = 'Описание недоступно';
+  if (pers === character.type) {
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < special.length; i++) {
+      // eslint-disable-next-line eqeqeq
+      if (special[i].description == undefined) {
+        special[i].description = 'Описание недоступно';
+      }
     }
+    return special;
   }
-  return special;
-  // }
+  throw new Error('Ошибка');
 }
